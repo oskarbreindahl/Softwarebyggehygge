a53665cef74ef20e8479e3f0e26d5874ed7af0b0
Oskar Breindahl <osbr@itu.dk>
2024-03-22 15:46:35 +0100

    Create whole prototype

--- SkyGate_Prototype/frontend/src/App.js
+++ SkyGate_Prototype/frontend/src/App.js
@@ -1,19 +1,74 @@
 import React, { useState } from 'react';
-import {Container, Button, Stack } from '@mui/material';
+import {Container, Button, Stack, Typography, TextField } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import axios from 'axios';
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [id, setID] = useState('');
+  const [boardingPass, setBoardingPass] = useState('');
+  const [resultMessage, setResultMessage] = useState('');
 
-  const handleClick = async () => {
+  const getPassButton = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/pass');
+      const response = await axios.get('http://localhost:3001/getPass/' 
+      + first_name + '/'
+      + last_name + '/' 
+      + id);
       console.log(response.data)
-      setMessage(JSON.stringify(response.data));
+      setBoardingPass(JSON.stringify(response.data));
     } catch (error) {
       console.error('Error fetching message:', error);
     }
+  };
+  const verifyPassButton = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/verifyPass/'
+      + first_name + '/'
+      + last_name + '/' 
+      + id);
+      console.log(response.data)
+      const res = JSON.stringify(response.data)
+      setResultMessage(JSON.stringify(response.data));
+
+    } catch (error) {
+      console.error('Error fetching message:', error);
+    }
+  };
+  const corruptPassButton = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/verifyPass/'
+      + first_name + '/'
+      + last_name + '/' 
+      + id + "1298312983");
+      console.log(response.data)
+      const res = JSON.stringify(response.data)
+      setResultMessage(JSON.stringify(response.data));
+
+    } catch (error) {
+      console.error('Error fetching message:', error);
+    }
+  };
+  const corruptDBButton = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/corruptDB/'
+      + first_name + '/'
+      + last_name + '/' 
+      + id);
+      console.log(response.data)
+    } catch (error) {
+      console.error('Error fetching message:', error);
+    }
+  };
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+  const handleIDChange = (event) => {
+    setID(event.target.value);
   };
 
   return (
@@ -24,12 +79,32 @@ function App() {
       paddingTop: 50,
     }}>
       <Stack direction="column" spacing={2} >
-        <h1>React Frontend</h1>
-        <Button variant="outlined" endIcon={<SendIcon />} onClick={handleClick} style={{
+        <Typography variant="h2" gutterBottom>
+          SkyGate Boarding Pass Service
+        </Typography>
+        <TextField id="standard-basic" onChange={handleFirstNameChange} value={first_name} label="First name" variant="standard" />
+        <TextField id="standard-basic" onChange={handleLastNameChange} value={last_name} label="Last name" variant="standard" />
+        <TextField id="standard-basic" onChange={handleIDChange} value={id} label="ID" variant="standard" />
+        <Button variant="outlined" onClick={getPassButton} style={{
           display: 'center',
           alignItems: 'center',
           }}>Get boarding pass</Button>
-        <p>Received message: {message}</p>
+        <Typography variant="p" gutterBottom>
+          Your boarding pass: {boardingPass}
+        </Typography>  
+        <Button variant="outlined" onClick={verifyPassButton} style={{
+          display: 'center',
+          alignItems: 'center',
+          }}>Verify boarding pass</Button>
+          <Button variant="outlined" onClick={corruptPassButton} style={{
+          display: 'center',
+          alignItems: 'center',
+          }}>Corrupt boarding pass</Button>
+        <p>Verified: {resultMessage}</p> 
+        <Button variant="outlined" onClick={corruptDBButton} style={{
+          display: 'center',
+          alignItems: 'center',
+          }}>Corrupt database</Button>       
       </Stack>
     </Container>
   );
--- SkyGate_Prototype/server.js
+++ SkyGate_Prototype/server.js
@@ -5,13 +5,24 @@ const { json } = require('express/lib/response');
 const app = express();
 app.use(cors());
 
-app.get('/pass', (req, res) => {
-  res.send(issueBoardingPass("Oskar", "Breindahl", 1010101010));
+var db = new Map()
+var db_backup = new Map()
+var boardingpass_status = new Map()
+
+app.get('/getPass/:name/:last/:id', (req, res) => {
+  res.send(issueBoardingPass(req.params.name, req.params.last, req.params.id))
+});
+
+app.get('/verifyPass/:name/:last/:id', (req, res) => {
+  res.send(verifyBoardingPass(req.params.name, req.params.last, req.params.id));
+});
+
+app.get('/corruptDB/:name/:last/:id', (req, res) => {
+  db.set(req.params.name + req.params.last + req.params.id, "corrupted")
 });
 
 // Start the server on port 3001
 app.listen(3001, () => {
-  console.log('Server is running on port 3001');
 });
 
 function issueBoardingPass(first_name, last_name, id){
@@ -19,21 +30,44 @@ function issueBoardingPass(first_name, last_name, id){
     return { error }
   }
 
-  dbEntry = first_name + ";" + last_name + ";" + id + ";issued" + "\n"
+  passenger = first_name + last_name + id
+  hash = hashString(passenger)
 
-  fs.appendFile('./db.txt', dbEntry, err => {
-    if (err) {
-      console.error(err);
-    }
-  });
+  db.set(passenger, hash)
+  db_backup.set(passenger, hash)
+  boardingpass_status.set(hash, "issued")
 
   return {
     first_name : first_name,
     last_name : last_name,
-    id : id
+    id : id,
   }
 }
 
 function verifyPerson(first_name, last_name, id) {
   return true
 }
+
+function verifyBoardingPass(first_name, last_name, id) {  
+  passenger = first_name + last_name + id
+  hash = hashString(passenger)
+  if (checkHash(passenger) && (boardingpass_status.get(hash) === "issued")) {
+    return true
+  }
+  return false
+}
+
+function hashString(inputString) {
+  hash = inputString.length * 4321 % 425 + "";
+  return hash
+}
+
+function checkHash(inputString) {
+  hashedstring = hashString(inputString)
+  db_entry = db.get(inputString)
+   if (hashedstring === db_entry) {
+    return true
+   }
+   return hashedstring === db_backup.get(inputString)
+}
+
